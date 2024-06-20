2338913f8b39a2e66fcbc696eda9535d492d8ca0
Gurly08 <bagas.dj89@gmail.com>
2024-06-20 23:37:59 +0700

    update

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\StoreUserRequest; // Namespace yang diperbaiki
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        // $users = \App\Models\User::paginate(10);
+        $users = DB::table('users')
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('name', 'like', '%' . $name . '%');
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.user.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->all();
+        $data['password'] = Hash::make($request->password);
+        \App\Models\User::create($data);
+        return redirect()->route('user.index')->with('success', 'User successfully created');
+    }
+}
