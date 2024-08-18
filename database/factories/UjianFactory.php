bc30125fa1b8114e7a85a2d651068ecd5f621736
Gurly08 <bagas.dj89@gmail.com>
2024-08-18 15:29:24 +0700

    fix typo on migrate

--- database/factories/UjianFactory.php
+++ database/factories/UjianFactory.php
@@ -22,12 +22,12 @@ class UjianFactory extends Factory
             'nilai_penyebab_kehamilan' => $this->faker->numberBetween(1,100),
             'nilai_perubahan_emosi' => $this->faker->numberBetween(1,100),
             'hasil' => $this->faker->randomElement(['Sangat Paham', 'Kurang Paham', 'Tidak Memahami']),
-            'status_kesehatan_reproduksi' => $this->faker->randomElement(['start, done']),
-            'status_penyebab_kehamilan' => $this->faker->randomElement(['start, done']),
-            'status_perubahan_emosi' => $this->faker->randomElement(['start, done']),
+            'status_kesehatan_reproduksi' => $this->faker->randomElement(['start', 'done']),
+            'status_penyebab_kehamilan' => $this->faker->randomElement(['start', 'done']),
+            'status_perubahan_emosi' => $this->faker->randomElement(['start', 'done']),
             'timer_kesehatan_reproduksi' => $this->faker->numberBetween(1,100),
             'timer_penyebab_kehamilan' => $this->faker->numberBetween(1,100),
-            'imer_perubahan_emosi' => $this->faker->numberBetween(1,100),
+            'timer_perubahan_emosi' => $this->faker->numberBetween(1,100),
         ];
     }
 }
--- database/migrations/2024_06_23_112215_create_ujians_table.php
+++ database/migrations/2024_06_23_112215_create_ujians_table.php
@@ -20,9 +20,9 @@ return new class extends Migration
             $table->integer('nilai_penyebab_kehamilan')->nullable();
             $table->integer('nilai_perubahan_emosi')->nullable();
             //status ujian enum (start, done)
-            $table->enum('status_kesehatan_reproduksi', ['start, done'])->default('start');
-            $table->enum('status_penyebab_kehamilan', ['start, done'])->default('start');
-            $table->enum('status_perubahan_emosi', ['start, done'])->default('start');
+            $table->enum('status_kesehatan_reproduksi', ['start', 'done'])->default('start');
+            $table->enum('status_penyebab_kehamilan', ['start', 'done'])->default('start');
+            $table->enum('status_perubahan_emosi', ['start', 'done'])->default('start');
             //timer ujian perkategori
             $table->integer('timer_kesehatan_reproduksi')->nullable();
             $table->integer('timer_penyebab_kehamilan')->nullable();
